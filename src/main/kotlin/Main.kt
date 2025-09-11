adf5bb7f850b8e35863917123882a0a051ccff3b
Harshavardhan <harshagowdaout987@gmail.com>
2025-09-11 08:27:31 +0530

    diffent modle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,43 @@
 import java.util.*
+import kotlin.math.exp
 
-fun main () {
+fun main() {
     println("Welcome to expression evaluator cli \" Type exit to exit \" ")
     val scanner = Scanner(System.`in`)
 
+    while (true) {
+        println("Enter the EXP>")
+        val input = scanner.nextLine()
 
+        if (input.lowercase() == "exit") {
+            println("Sayonara 👋")
+            break
+        }
+
+        try {
+            val result = evaluateExp(input)
+            println("= $result")
+        } catch (e: Exception) {
+            println(" ❌ Error: ${e.message}")
+        }
+
+    }
+}
+
+fun evaluateExp(expr: String) : Double {
+    val tokens = tokenize(expr)
+    val postfix = infixToPostfix(tokens)
+    return evaluatePostfix(postfix)
+}
+
+fun tokenize (expr: String): List<String> {
+    val tokens = mutableListOf<String>()
+    val number = " "
+
+    for (ch in expr) {
+        when {
+            ch.isDigit() || ch = '.' -> number += ch
+            ch == ' ' -> continue
+        }
+    }
 }
