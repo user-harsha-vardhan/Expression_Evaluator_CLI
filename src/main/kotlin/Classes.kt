ca7c6bce0f0251e0a82b622850908a659d0b70f4
Harshavardhan <harshagowdaout987@gmail.com>
2025-09-11 10:06:13 +0530

    Function parse added to class file

--- src/main/kotlin/Classes.kt
+++ src/main/kotlin/Classes.kt
@@ -1,9 +1,9 @@
 sealed class Expr {
-    data class Number(val value: Double) :Expr()
-    data class  Add(val left: Expr, val right: Expr) : Expr()
-    data class  Subtract(val left: Expr, val right: Expr) : Expr()
-    data class  Multiply(val left: Expr, val right: Expr) : Expr()
-    data class  Divide(val left: Expr, val right: Expr) : Expr()
+    data class Number(val value: Double) : Expr()
+    data class Add(val left: Expr, val right: Expr) : Expr()
+    data class Subtract(val left: Expr, val right: Expr) : Expr()
+    data class Multiply(val left: Expr, val right: Expr) : Expr()
+    data class Divide(val left: Expr, val right: Expr) : Expr()
 
 }
 
@@ -13,4 +13,22 @@ fun eval(expr: Expr): Double = when (expr) {
     is Expr.Subtract -> eval(expr.left) - eval(expr.right)
     is Expr.Multiply -> eval(expr.left) * eval(expr.right)
     is Expr.Divide -> eval(expr.left) - eval(expr.right)
+}
+
+fun parse(tokens: List<String>): Expr {
+    if (tokens.size == 1) return Expr.Number(tokens[0].toDouble())
+
+    var index = tokens.indexOfFirst { it == "*" || it == "/" }
+    if (index != -1) {
+        val left = parse(tokens.subList(0, index))
+        val right = parse(tokens.subList(index + 1, tokens.size))
+        return if (tokens[index] == "*") Expr.Multiply(left, right) else Expr.Divide(left, right)
+    }
+    index = tokens.indexOfFirst { it == "+" || it == "-" }
+    if (index != -1) {
+        val left = parse(tokens.subList(0, index))
+        val right = parse(tokens.subList(index + 1, tokens.size))
+        return if (tokens[index] == "+" ) Expr.Add(left, right) else Expr.Subtract(left, right)
+    }
+    throw IllegalArgumentException ("Invalid expression: $tokens")
 }
