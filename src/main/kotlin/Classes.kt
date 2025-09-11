0dfea0e6c6df10078e35e003fee939e3eaa62bc3
Harshavardhan <harshagowdaout987@gmail.com>
2025-09-11 08:50:41 +0530

    eval function is added

--- src/main/kotlin/Classes.kt
+++ src/main/kotlin/Classes.kt
@@ -6,3 +6,11 @@ sealed class Expr {
     data class  Divide(val left: Expr, val right: Expr) : Expr()
 
 }
+
+fun eval(expr: Expr): Double = when (expr) {
+    is Expr.Number -> expr.value
+    is Expr.Add -> eval(expr.left) + eval(expr.right)
+    is Expr.Subtract -> eval(expr.left) - eval(expr.right)
+    is Expr.Multiply -> eval(expr.left) * eval(expr.right)
+    is Expr.Divide -> eval(expr.left) - eval(expr.right)
+}
