25007e58d229d9474ddaa42dacfcf4f49ded1fc7
Harshavardhan <harshagowdaout987@gmail.com>
2025-09-11 10:40:17 +0530

    main function added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
-import java.util.*
-import kotlin.math.exp
-
 fun main() {
+    println("Wellcome to expression evaluator")
+
+    while (true) {
+    println("enter the expression:")
+        val input = readLine() ?: break
+        if (input.lowercase() == "exit") break
+
+        try {
+            val tokens = input.split(" ").filter { it.isNotBlank() }
+            val expr = parse(tokens)
+            val result = eval(expr)
+            println("Result: $result")
+        } catch (e: Exception) {
+            println("❌Error: ${e.message}")
+        }
+    }
 }
